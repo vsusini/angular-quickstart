67f2ecc34e2d8fd88777ed7789eb454df630db56
Vince <31858345+vsusini@users.noreply.github.com>
2023-01-18 11:01:25 -0500

    another attempt

--- server.js
+++ server.js
@@ -1,39 +1,6 @@
-const express = require('express');
-const request = require('request');
-const path = require('path');
-const cors = require('cors')
-const port = process.env.PORT || 4500;
-var bodyParser = require('body-parser')
-const app = express();
-const publicPath = path.join(__dirname, "/dist/rs-dashboard/")
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
 
-// parse application/json
-app.use(bodyParser.json())
+// server.js
 
-// middle ware
-app.use(express.static(publicPath));
-app.use(express.static('public')); //to access the files in public folder
-app.use(cors()); // it enables all cors requests
+const app = require("./express/server");
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
-
-app.post('/getAccount', (req, res) => {
-    request(
-      { url: 'http://public-api.solscan.io/account/' + req.body.wallet},
-      (error, response, body) => {
-        if (error || response.statusCode !== 200) {
-          return res.status(500).json({ type: 'error', message: err.message });
-        }
-  
-        res.json(JSON.parse(body));
-      }
-    )
-  });
-
-app.listen(port, () => {
-    console.log('server is running at port ' + port);
-})
+app.listen(3000, () => console.log("Local app listening on port 3000!"));
